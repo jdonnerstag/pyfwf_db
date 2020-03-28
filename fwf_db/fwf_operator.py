cb910be96b6bf7a9a652f2f563362f97e109c590
Juergen Donnerstag <juergen.donnerstag@dxc.com>
2020-03-28 13:12:26 +0100

    The new perator is really nice

---
+++ fwf_db/fwf_operator.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import sys
+
+
+class FWFOperator(object):
+
+    def __init__(self, name, func=None):
+        self.name = name
+        self.func = func
+        
+        if func is None:
+            self.func = lambda x: x
+
+    def get(self, line):
+        return self.func(line[self.name])
+
+    def __eq__(self, other):
+        return lambda line: self.get(line) == other
+    
+    def __ne__(self, other):
+        return lambda line: self.get(line) != other
+    
+    def __gt__(self, other):
+        return lambda line: self.get(line) > other
+    
+    def __lt__(self, other):
+        return lambda line: self.get(line) < other
+    
+    def __ge__(self, other):
+        return lambda line: self.get(line) >= other
+
+    def __le__(self, other):
+        return lambda line: self.get(line) <= other
+
+    def is_in(self, other):
+        return lambda line: self.get(line) in other
+
+    def is_notin(self, other):
+        return lambda line: self.get(line) not in other
+
+    def str(self, encoding=None):
+        self.encoding = encoding or sys.getdefaultencoding()
+        orig = self.func
+        self.func = lambda x: str(orig(x), self.encoding)
+        return self
+
+    def strip(self):
+        orig = self.func
+        self.func = lambda x: orig(x).strip()
+        return self
+
+    def lower(self):
+        orig = self.func
+        self.func = lambda x: orig(x).lower()
+        return self
+
+    def upper(self):
+        orig = self.func
+        self.func = lambda x: orig(x).upper()
+        return self
+
+    def int(self):
+        orig = self.func
+        self.func = lambda x: int(orig(x))
+        return self
---
+++ tests/test_operator.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import pytest
+
+import os
+import sys
+import io
+import numpy as np
+
+from fwf_db import FWFFile, FWFSimpleIndex, FWFMultiFile, FWFUnique
+from fwf_db.fwf_unique_np_based import FWFUniqueNpBased
+from fwf_db.fwf_index_np_based import FWFIndexNumpyBased
+from fwf_db.fwf_operator import FWFOperator
+
+
+DATA = b"""# My comment test
+US       AR19570526Fbe56008be36eDianne Mcintosh         Whatever    Medic        #
+US       MI19940213M706a6e0afc3dRosalyn Clark           Whatever    Comedian     #
+US       WI19510403M451ed630accbShirley Gray            Whatever    Comedian     #
+US       MD20110508F7e5cd7324f38Georgia Frank           Whatever    Comedian     #
+US       PA19930404Mecc7f17c16a6Virginia Lambert        Whatever    Shark tammer #
+US       VT19770319Fd2bd88100facRichard Botto           Whatever    Time traveler#
+US       OK19910917F9c704139a6e3Alberto Giel            Whatever    Student      #
+US       NV20120604F5f02187599d7Mildred Henke           Whatever    Super hero   #
+US       AR19820125Fcf54b2eb5219Marc Kidd               Whatever    Medic        #
+US       ME20080503F0f51da89a299Kelly Crose             Whatever    Comedian     #
+"""
+
+
+class HumanFile(object):
+
+    FIELDSPECS = [
+        {"name": "location", "len": 9},
+        {"name": "state", "len": 2},
+        {"name": "birthday", "len": 8},
+        {"name": "gender", "len": 1},
+        {"name": "name", "len": 36},
+        {"name": "universe", "len": 12},
+        {"name": "profession", "len": 13},
+        {"name": "dummy", "len": 1},
+    ]
+
+
+
+def test_table_filter_by_line():
+    fwf = FWFFile(HumanFile)
+    with fwf.open(DATA):
+
+        op = FWFOperator
+        rtn = fwf.filter(op("gender") == b"M")
+        assert len(list(rtn)) == 3
+        assert len(rtn) == 3
+
+        rtn = fwf.filter(op("gender") != b"M")
+        assert len(list(rtn)) == 7
+        assert len(rtn) == 7
+
+        rtn = fwf.filter(op("gender") > b"F")
+        assert len(list(rtn)) == 3
+        assert len(rtn) == 3
+
+        rtn = fwf.filter(op("gender") < b"M")
+        assert len(list(rtn)) == 7
+        assert len(rtn) == 7
+
+        rtn = fwf.filter(op("gender").str() == "F")
+        assert len(rtn) == 7
+
+        rtn = fwf.filter(op("gender").str().strip() == "F")
+        assert len(rtn) == 7
+
+        rtn = fwf.filter(op("gender").str().lower() == "f")
+        assert len(rtn) == 7
+
+        rtn = fwf.filter(op("birthday").int() < 19600000)
+        assert len(rtn) == 2
+
+        birthday_year = op("birthday")
+        birthday_year.func = lambda x: int(x) / 100 / 100
+        rtn = fwf.filter(birthday_year < 1960)
+
+        birthday_year = op("birthday", lambda x: int(x) / 100 / 100)
+        rtn = fwf.filter(birthday_year < 1960)
+
+# Note: On Windows all of your multiprocessing-using code must be guarded by if __name__ == "__main__":
+if __name__ == '__main__':
+
+    test_table_filter_by_line()
